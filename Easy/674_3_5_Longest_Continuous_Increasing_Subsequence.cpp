4b7715461184f5c9822ca4325b6594eb7f245d41
Shura <petrovai_net@mail.ru>
2024-02-04 14:46:03 +0300

    added to folder Easy 674 Longest Continuous Increasing Subsequence

---
+++ Easy/674_3_5_Longest_Continuous_Increasing_Subsequence.cpp
@@ -0,0 +1,96 @@
+#include <cstdio>   
+#include <cstdlib> 
+#include <cassert>
+#include <deque>
+#include <ios>
+#include <iostream>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <vector>
+#include <bitset>
+#include <array>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <set>
+#include <string>
+
+#pragma push_macro("assert")
+#undef assert
+#pragma pop_macro("assert")
+
+const int FASTIO = []() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
+// Definition for singly - linked list.
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// my version
+
+// 1 version
+class Solution 
+{
+public:
+    int findLengthOfLCIS(const std::vector<int>& a) 
+	{
+		int bestLen = 0;
+		int curLen = 0;
+		int prev = INT_MIN;
+		for (int v : a)
+		{
+			if (v > prev)
+				curLen++;
+			else
+				curLen = 1;
+			bestLen = std::max(bestLen, curLen);
+			prev = v;
+		}
+		return bestLen;
+    }
+};
+
+// 2 version
+
+// 3 version
+
+// 4 version
+
+// 5 version
+
+// 6 version
+
+// 7 version
+
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[])
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+
+	return EXIT_SUCCESS;
+}
