97d887932024de6b5dcfc2cab489fbef68a560bc
Shura <petrovai_net@mail.ru>
2024-02-04 14:22:09 +0300

    Added to folder Easy 563 Binary Tree Tilt

---
+++ Easy/563_3_5_Binary_Tree_Tilt.cpp
@@ -0,0 +1,111 @@
+#include <cstdio>   
+#include <cstdlib> 
+#include <cassert>
+#include <deque>
+#include <ios>
+#include <iostream>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <vector>
+#include <bitset>
+#include <array>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <set>
+#include <string>
+
+#pragma push_macro("assert")
+#undef assert
+#pragma pop_macro("assert")
+
+//#pragma GCC target("popcnt")
+
+const int FASTIO = []() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
+// Definition for singly - linked list.
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// https://
+
+// my version
+
+// 1 version
+class Solution 
+{
+	struct Stat
+	{
+		int valuesSum;
+		int tiltSum;
+	};
+
+	Stat getStat(TreeNode* node)
+	{
+		if (node == nullptr)
+			return Stat{ 0, 0 };
+		Stat left = getStat(node->left);
+		Stat right = getStat(node->right);
+		int tilt = std::abs(left.valuesSum - right.valuesSum);
+		return Stat{
+			left.valuesSum + right.valuesSum + node->val,
+			left.tiltSum + right.tiltSum + tilt
+		};
+	}
+
+public:
+    int findTilt(TreeNode* root) 
+	{	
+		return getStat(root).tiltSum;
+    }
+}; 
+
+// 2 version
+
+// 3 version
+
+// 4 version
+
+// 5 version
+
+// 6 version
+
+// 7 version
+
+// 8 version
+
+// 9 version
+
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[])
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+
+	return EXIT_SUCCESS;
+}
