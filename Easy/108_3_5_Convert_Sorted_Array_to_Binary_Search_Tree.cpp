5b981756463f34e14b4ae092a374800e3ae74726
Shura <petrovai_net@mail.ru>
2024-02-04 05:35:23 +0300

    added to folder easy 108 Convert Sorted Array to Binary

---
+++ Easy/108_3_5_Convert_Sorted_Array_to_Binary_Search_Tree.cpp
@@ -0,0 +1,104 @@
+#include <cstdio>   
+#include <cstdlib> 
+#include <cassert>
+#include <deque>
+#include <ios>
+#include <iostream>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <vector>
+#include <bitset>
+#include <array>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <set>
+#include <string>
+#include <functional>
+
+#pragma push_macro("assert")
+#undef assert
+#pragma pop_macro("assert")
+
+//#pragma GCC target("popcnt")
+
+const int FASTIO = []() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
+// Definition for singly - linked list.
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+// https://www.youtube.com/watch?v=kwvdyTNC6IQ
+
+// my version
+
+// 1 version
+class Solution 
+{
+	TreeNode* getBST(const std::vector<int>& nums, int left, int right)
+	{
+		if (left > right)
+			return nullptr;
+		int mid = (left + right) / 2;
+		TreeNode * root = new TreeNode(nums[mid]);
+		root->left = getBST(nums, left, mid - 1);
+		root->right = getBST(nums, mid + 1, right);
+		return root;
+	}
+
+public:
+    TreeNode* sortedArrayToBST(const std::vector<int>& nums) 
+	{
+		return getBST(nums, 0, (int)nums.size() - 1);
+    }
+};
+
+// 2 version
+
+// 3 version
+
+// 4 version
+
+// 5 version
+
+// 6 version
+
+// 7 version
+
+// 8 version
+
+// 9 version
+
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[])
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+
+	return EXIT_SUCCESS;
+}
